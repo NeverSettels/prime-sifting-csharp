b66a954740cfbf742a0b80754434f30820f4cabf
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-05-06 13:51:55 -0700

    continue trying

--- Models/prime-sifting.cs
+++ Models/prime-sifting.cs
@@ -8,27 +8,32 @@ namespace Prime.Models
     public static List<int> PrimeFunction(int input)
     {
       List<int> found = new List<int> { };
+      List<int> primeList = new List<int> { };
       int prime = 2;
       for (int i = 2; i <= input; i++)
       {
         found.Add(i);
       }
-
-      for (int i = found.Count - 1; i > 2; i--)
+      // Console.WriteLine(found.Count);
+      int counter = found.Count;
+      for (int i = counter - 1; i >= 2; i--)
       {
-        for (int j = prime; j < found.Count - 1; j++)
+        for (int j = prime; j < counter - 1; j++)
         {
-          if (i % j == 0 && i != j)
+          Console.WriteLine($"this is i:{i} this is j: {j}");
+          if (found[i] % j == 0 && found[i] != j)
           {
-            Console.WriteLine("This should be not prime " + j);
-            found.Remove(j);
+            //Console.WriteLine("This should be not prime " + found[i]);
+            //found.Remove(i); 
+          }
+          else
+          {
+            if (!primeList.Contains(j))
+              primeList.Add(j);
           }
         }
-
-
-
       }
-      return found;
+      return primeList;
     }
   }
 }
